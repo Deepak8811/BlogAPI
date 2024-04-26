0d15d7d531128ebe77eb00c6762e1bcde62363e8
Deepak chaurasiya <deepchaurasiya1997@gmail.com>
2024-04-26 12:08:13 +0530

    Backend for blog api is completed

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    dbName: process.env.DB_NAME,
+})
+    .then(() => {
+        console.log('MongoDB connected...!')
+    })
+    .catch(err => console.log(err.message));
